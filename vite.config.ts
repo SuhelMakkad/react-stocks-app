494e5e657cd36c892dd07fab8cfa66737d912c8f
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-07-13 22:59:59 +0530

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import path from "path";
+
+const getPath = (route: string) => path.resolve(__dirname, route);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@/pages": getPath("./src/pages"),
+      "@/components": getPath("./src/common/components"),
+      "@/hooks": getPath("./src/common/hooks"),
+      "@/styles": getPath("./src/common/styles"),
+      "@/lib": getPath("./src/common/lib"),
+      "@/store": getPath("./src/common/store"),
+      "@/images": getPath("./src/assets/images"),
+      "@/assets": getPath("./src/assets"),
+      "@/": getPath("./src"),
+    },
+  },
+
+  plugins: [react()],
+});
