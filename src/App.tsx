de73ff9683d893e484322d81f159721543db4c4c
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-07-14 23:37:35 +0530

    added skeleton for layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
-import "@/styles/global.css";
-import { ThemeToggle } from "@/components/theme-toggle";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <ThemeToggle />
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <main className="flex flex-col-reverse md:flex-row min-h-screen">
+      <aside className="border w-full md:w-80 min-h-[4rem]"></aside>
+      <div className="border grow min-h-[10rem]"></div>
+    </main>
   );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "@/components/theme-provider";
 
+import "@/styles/global.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
