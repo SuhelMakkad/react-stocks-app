ab2b271a0abf935de63982e2a54774f5f6df46e5
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-07-15 02:31:09 +0530

    added theme toggle on page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,16 @@
 import OverviewCard from "@/components/overview-card";
 
 import { overviewData } from "@/assets/data/overview";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Dashboard = () => {
   return (
     <div className="container mx-auto pt-8">
       <div className="flex justify-between items-center gap-2 mb-8">
         <h1 className="text-3xl font-semibold tracking-tight">Markets</h1>
-        <span className="text-muted-foreground">Quick overview of the market</span>
+        <span className="text-muted-foreground">
+          <ThemeToggle />
+        </span>
       </div>
 
       <div className="flex md:grid gap-2 md:gap-4 grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 overflow-auto hide-scroll">
