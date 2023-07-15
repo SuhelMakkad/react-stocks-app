9eaa8fba14fa24606f457211cabd58eb823b353a
SUHEL Makkad <osshomes2@gmail.com>
2023-07-15 16:53:00 +0530

    minor updates

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -7,7 +7,7 @@ export type MainLayoutProps = {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="flex flex-col-reverse lg:flex-row min-h-screen">
-      <aside className="w-full lg:w-72 min-h-[4rem] shrink-0 pt-2">
+      <aside className="w-full lg:w-72 min-h-[4rem] shrink-0">
         <Navbar />
       </aside>
 
