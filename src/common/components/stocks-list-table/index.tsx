03ec7de4f7e4400114863a2cbee213f2179385e6
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-07-15 13:15:16 +0530

    minor updates

--- src/common/components/stocks-list-table/index.tsx
+++ src/common/components/stocks-list-table/index.tsx
@@ -170,8 +170,7 @@ const StocksListTable = ({ stocks }: StocksListTableProps) => {
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
         <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredSelectedRowModel().rows.length} of{" "}
-          {table.getFilteredRowModel().rows.length} row(s) selected.
+          {stocks.length} of {table.getFilteredRowModel().rows.length} row(s).
         </div>
         <div className="space-x-2">
           <Button
