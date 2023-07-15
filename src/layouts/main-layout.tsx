d625ee396808207bfce14dce49ffef461a7b244d
SUHEL Makkad <osshomes2@gmail.com>
2023-07-16 01:05:45 +0530

    fixed side nav oveflow

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -7,7 +7,7 @@ export type MainLayoutProps = {
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="flex flex-col-reverse gap-4 md:gap-0 lg:flex-row min-h-screen">
-      <aside className="z-10 border-t lg:border-none sticky bottom-0 left-0 right-0 lg:static w-full lg:w-72 min-h-[4rem] shrink-0">
+      <aside className="z-10 border-t lg:border-none sticky bottom-0 left-0 right-0 md:top-0 w-full lg:w-72 min-h-[4rem] shrink-0 max-h-screen">
         <Navbar />
       </aside>
 
