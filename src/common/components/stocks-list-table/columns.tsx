258af6026a943edb6369a0e2ffce0588dd870f3a
SUHEL Makkad <MAKADSUHEL11@GMAIL.COM>
2023-07-16 14:41:31 +0530

    moved columns to different page

---
+++ src/common/components/stocks-list-table/columns.tsx
@@ -0,0 +1,77 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowDown, ArrowUp, ArrowUpDown } from "lucide-react";
+
+import TrendChip from "@/components/trend-chip";
+import OverviewChart from "@/components/chart/overview";
+import type { Stock } from "@/assets/data/stocks";
+
+import { formateAmount } from "@/utils/index";
+
+const SortArrow = ({ order }: { order?: string | false }) => {
+  if (!order) {
+    return <ArrowUpDown className="ml-2 h-3 w-3" />;
+  }
+
+  if (order === "asc") {
+    return <ArrowUp className="ml-2 h-3 w-3" />;
+  }
+  return <ArrowDown className="ml-2 h-3 w-3" />;
+};
+
+export const columns: ColumnDef<Stock>[] = [
+  {
+    accessorKey: "fullName",
+    header: ({ column }) => {
+      const sortOrder = column.getIsSorted();
+      return (
+        <button
+          className="flex gap-2 items-center"
+          onClick={() => column.toggleSorting(sortOrder === "asc")}
+        >
+          Name
+          <SortArrow order={sortOrder} />
+        </button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="flex flex-col capitalize">
+        <span className="text-lg font-medium uppercase">{row.original.symbol}</span>
+        <span className="text-secondary-foreground">{row.getValue("fullName")}</span>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "chartData",
+    header: () => {
+      return <span className="flex">Trend</span>;
+    },
+    cell: ({ row }) => (
+      <OverviewChart chartData={row.getValue("chartData")} trend={row.original.trend} />
+    ),
+  },
+  {
+    accessorKey: "currentPrice",
+    header: ({ column }) => {
+      const sortOrder = column.getIsSorted();
+      return (
+        <button
+          className="flex gap-2 items-center"
+          onClick={() => column.toggleSorting(sortOrder === "asc")}
+        >
+          Amount
+          <SortArrow order={sortOrder} />
+        </button>
+      );
+    },
+    cell: ({ row }) => (
+      <div className="flex flex-col md:flex-row gap-1 items-end md:items-center lowercase">
+        <span className="text-lg font-medium">{formateAmount(row.getValue("currentPrice"))}</span>
+        <TrendChip
+          className="grow-0 w-max scale-90"
+          trend={row.original.trend}
+          percentageChange={row.original.percentageChange}
+        />
+      </div>
+    ),
+  },
+];
--- src/common/components/stocks-list-table/index.tsx
+++ src/common/components/stocks-list-table/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Search } from "lucide-react";
 import {
-  ColumnDef,
   ColumnFiltersState,
   SortingState,
   VisibilityState,
@@ -10,7 +12,6 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
-import { ArrowDown, ArrowUp, ArrowUpDown, Search } from "lucide-react";
 
 import {
   Table,
@@ -20,86 +21,13 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { columns } from "./columns";
 
-import TrendChip from "@/components/trend-chip";
 import type { Stock } from "@/assets/data/stocks";
-
-import { formateAmount } from "@/utils/index";
-import OverviewChart from "../chart/overview";
-import { useNavigate } from "react-router-dom";
 
 export type StocksListTableProps = {
   stocks: Stock[];
 };
-
-const SortArrow = ({ order }: { order?: string | false }) => {
-  if (!order) {
-    return <ArrowUpDown className="ml-2 h-3 w-3" />;
-  }
-
-  if (order === "asc") {
-    return <ArrowUp className="ml-2 h-3 w-3" />;
-  }
-  return <ArrowDown className="ml-2 h-3 w-3" />;
-};
-
-export const columns: ColumnDef<Stock>[] = [
-  {
-    accessorKey: "fullName",
-    header: ({ column }) => {
-      const sortOrder = column.getIsSorted();
-      return (
-        <button
-          className="flex gap-2 items-center"
-          onClick={() => column.toggleSorting(sortOrder === "asc")}
-        >
-          Name
-          <SortArrow order={sortOrder} />
-        </button>
-      );
-    },
-    cell: ({ row }) => (
-      <div className="flex flex-col capitalize">
-        <span className="text-lg font-medium uppercase">{row.original.symbol}</span>
-        <span className="text-secondary-foreground">{row.getValue("fullName")}</span>
-      </div>
-    ),
-  },
-  {
-    accessorKey: "chartData",
-    header: () => {
-      return <span className="flex">Trend</span>;
-    },
-    cell: ({ row }) => (
-      <OverviewChart chartData={row.getValue("chartData")} trend={row.original.trend} />
-    ),
-  },
-  {
-    accessorKey: "currentPrice",
-    header: ({ column }) => {
-      const sortOrder = column.getIsSorted();
-      return (
-        <button
-          className="flex gap-2 items-center"
-          onClick={() => column.toggleSorting(sortOrder === "asc")}
-        >
-          Amount
-          <SortArrow order={sortOrder} />
-        </button>
-      );
-    },
-    cell: ({ row }) => (
-      <div className="flex flex-col md:flex-row gap-1 items-end md:items-center lowercase">
-        <span className="text-lg font-medium">{formateAmount(row.getValue("currentPrice"))}</span>
-        <TrendChip
-          className="grow-0 w-max scale-90"
-          trend={row.original.trend}
-          percentageChange={row.original.percentageChange}
-        />
-      </div>
-    ),
-  },
-];
 
 const StocksListTable = ({ stocks }: StocksListTableProps) => {
   const [sorting, setSorting] = React.useState<SortingState>([]);
